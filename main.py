f5f9f58febfe0ee1d237538e3231758badf4e911
kraglik <igor.kraglik@yandex.ru>
2019-05-25 14:13:45 +0500

    ping-pong example updated

--- main.py
+++ main.py
@@ -14,38 +14,31 @@ class Ping(actor.Actor):
     def __init__(self, *args, **kwargs):
         super(Ping, self).__init__(*args, **kwargs)
         self.pong_ref = None
-        self.n = 100_000
 
     def receive(self, message, sender):
 
         if isinstance(message, str):
-            # print(message)
+            print(message)
 
-            if self.n == 0:
-                self.kill(self.pong_ref)
-
-                del self.pong_ref
-
-            else:
-                yield self.send(self.pong_ref, 'ping')
-                self.n -= 1
+            self.kill(self.pong_ref)
+            del self.pong_ref
 
         elif isinstance(message, system_messages.StartupMessage):
             self.pong_ref = yield self.spawn(Pong)
 
-            # print('ping')
+            print('ping')
+
             yield self.send(self.pong_ref, 'ping')
-            self.n -= 1
 
 
 def main():
     system = actor.ActorSystem()
     system.run()
 
-    for i in range(8):
+    for i in range(1_000):
         system.spawn(Ping)
 
-    time.sleep(60)
+    time.sleep(5)
 
     system.stop()
 
